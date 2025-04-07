0f758e12790af901183d27557d9def61ff70658b
Guilherme Lima <guilherme.lima@rioave.com.br>
2025-04-07 15:16:38 -0300

    primeiro desafio da pt 2 de logica

---
+++ app.js
@@ -0,0 +1,23 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio!';
+
+//  function verificarChute(){
+//      console.log("O botao foi clicado")
+//  }
+
+// function botaoAlerta(){
+//     console.log("Eu amo javaScipt")
+// }
+
+//  function promptClicado(){
+//      cidadeBrasil = prompt("Informe um nome de uma cidade do Brasil: ")
+
+//      alert(`Estive em ${cidadeBrasil} e lembrei de você`)
+//  }
+
+function somaDoisNum(){
+    let n1 = parseInt(prompt("Informe um número"))
+    let n2 = parseInt(prompt("Informe outro número"))
+    soma = n1 + n2
+    alert(soma)
+}
